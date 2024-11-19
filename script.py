af0b5a8b0048571af667f77e194fc6ffa5acc334
hiro-var <e123141@m.chukyo-u.ac.jp>
2024-11-20 00:48:45 +0900

    Add files via upload

---
+++ script.py
@@ -0,0 +1,18 @@
+math = 82
+japanese = 90
+english = 70
+totle_score = (math + japanese + english) / 3
+print(totle_score)
+
+price = 100
+text = f"この商品は{price}円です"
+print(text)
+
+surname = "佐野"
+given_name = "裕務"
+full_name = surname + given_name
+print(full_name)
+
+student_name = ["sano","saitou","oota","nisida","sasaki"]
+a = student_name[0]
+y = len(a)
